e9899512851fa56bdfb188db1a59d83bdaba32e1
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-21 08:04:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
-
+      item_name = req.path.split("/item/").last
+      
       item = @@items.find{|i| i.name == item_name}
-
+      
       if item.nil?
         resp.write "Item not found"
         resp.status = 400
@@ -20,8 +20,10 @@ class Application
     else
       resp.write "Route not found"
       resp.status = 404
-    end
 
+      
+    end
+    
     resp.finish
   end
 end

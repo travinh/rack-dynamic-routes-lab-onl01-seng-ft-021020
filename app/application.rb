e3ae4306c73a2a17c77feaaf1c018a4294db554b
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-21 08:03:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,16 @@ class Application
       item_name = req.path.split("/item/").last
       
       item = @@items.find{|i| i.name == item_name}
+      
+      if item.nil?
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write item.price
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
 
       
     end

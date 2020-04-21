bcb08e7049fb32e9e047579b266706449fd2aae2
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-21 08:03:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Request(env)
+    req = Rack::Request.new(env)
     
     if req.path.match(/items/)
       item_name = req.path.split("/item/").last
